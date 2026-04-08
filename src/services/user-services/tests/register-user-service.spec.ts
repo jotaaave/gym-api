ac296b66628817bb8e89f4d717d9f25fea3438fd
jotaaave <marbenebela@gmail.com>
2026-04-08 17:50:41 -0300

    test: added password hash test

--- src/services/user-services/tests/register-user-service.spec.ts
+++ src/services/user-services/tests/register-user-service.spec.ts
@@ -4,6 +4,7 @@ import type RegisterUserService from '../register-user-service';
 import InMemoryUserRepository from '../../../repositories/in-memory/user-repository-memory';
 import UserAlreadyExists from '../../../errors/UserAlreadyExists';
 import type { UserRepository } from '../../../repositories/types/user-repository';
+import { compare } from 'bcrypt';
 
 let usr: UserRepository;
 let sut: RegisterUserService;
@@ -18,7 +19,7 @@ describe('Register User Service', () => {
     const fakeUser = {
       name: "João",
       email: 'test@example.com',
-      password: 'password123',
+      passwordHash: 'password123',
     };
 
     const { user } = await sut.execute(fakeUser);
@@ -26,11 +27,25 @@ describe('Register User Service', () => {
     expect(user.email).toBe(fakeUser.email);
   });
 
+  it('should be able to hash the password', async () => {
+    const fakeUser = {
+      name: "João",
+      email: 'test@example.com',
+      passwordHash: 'password123',
+    };
+
+    const { user } = await sut.execute(fakeUser);
+
+    const passwordCompare = await compare(fakeUser.passwordHash, user.passwordHash);
+
+    expect(passwordCompare).toBe(true);
+  });
+
   it('should not be able to register a user with an existing email', async () => {
     const fakeUser = {
       name: "João",
       email: 'test@example.com',
-      password: 'password123',
+      passwordHash: 'password123',
     };
 
     await sut.execute(fakeUser);
