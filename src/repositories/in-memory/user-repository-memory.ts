a4d291b44bdf0d693b9e249b7d580cff4eff5e92
jotaaave <marbenebela@gmail.com>
2026-04-08 17:09:38 -0300

    refactor: changed method to create user using new type

--- src/repositories/in-memory/user-repository-memory.ts
+++ src/repositories/in-memory/user-repository-memory.ts
@@ -1,19 +1,26 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import type { User } from "../../@types/user";
 import type { UserRepository } from "../types/user-repository";
+import { v4 as uuidv4 } from "uuid";
 
 export default class InMemoryUserRepository implements UserRepository {
   private users: User[] = [];
 
   constructor() {}
 
-  async create(user: User) {
-    this.users.push(user);
+  async create(user: Omit<User, "id" | "createdAt" | "updatedAt" | "checkIns">): Promise<User> {
+    const newUser: User = {
+      ...user,
+      id: uuidv4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
 
-    return user;
+    this.users.push(newUser);
+
+    return newUser;
   }
 
-  async findByEmail(email: string) {
+  async findByEmail(email: string): Promise<User | null> {
     const user = this.users.find((user) => user.email === email);
 
     return user || null;
