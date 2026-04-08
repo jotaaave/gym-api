989a03a01403b5c0ff8d369c313cede3ad61185d
jotaaave <marbenebela@gmail.com>
2026-04-08 18:33:07 -0300

    feat(authenticate): added isPasswordValid to verify pass with bcrypt

--- src/services/user-services/authenticate-user-service.ts
+++ src/services/user-services/authenticate-user-service.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcrypt';
 import InvalidFormData from '../../errors/InvalidFormData';
 import type { UserRepository } from '../../repositories/types/user-repository';
 
@@ -13,6 +14,12 @@ export default class AuthenticateUserService {
     const user = await this.userRepository.findByEmail(data.email);
 
     if (!user) {
+      throw new InvalidFormData();
+    }
+
+    const isPasswordValid = await compare(data.password, user.passwordHash);
+
+    if (!isPasswordValid) {
       throw new InvalidFormData();
     }
 
